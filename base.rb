d26996c9586ed323bf617240129cc001e619f880
James W. Thompson, II <james@plainprograms.com>
2010-10-04 14:29:03 -0500

    Added additional test related gems. Added removal of prototype JS. Removed addition of jQuery UI component. Updated modifications to application.rb to match test libraries.

--- base.rb
+++ base.rb
@@ -1,36 +1,48 @@
 rvmrc = <<-RVMRC
-rvm_gemset_create_on_use_flag=1
-rvm gemset use #{app_name}
+rvm use 1.9.2@#{app_name} --create
 RVMRC
 
 create_file ".rvmrc", rvmrc
 
-gem "devise", "~> 1.1"
-gem "high_voltage", "~> 0.9"
-gem "paperclip", "~> 2.3"
-
-gem "shoulda", "~> 2.11", :group => :test
-gem "factory_girl_rails", "~> 1.0.0", :group => :test
+gem "test-unit",          :group => :test
+gem "shoulda",            :group => :test
+gem "factory_girl_rails", :group => :test
+gem "cucumber-rails",     :group => :test
+gem "capybara",           :group => :test
+gem "selenium-webdriver", :group => :test
+gem "spork",              :group => :test
+gem "launchy",            :group => :test
+gem "mocha",              :group => :test
+gem "simplecov",          :group => :test
 
 generators = <<-GENERATORS
 
     config.generators do |g|
-      g.test_framework :testunit, :fixture => true, :views => false
-      g.integration_tool :testunit, :fixture => true, :views => true
+      g.stylesheets false
+      g.test_framework :shoulda
+      g.fallbacks[:shoulda] = :test_unit
+      g.integration_tool :cucumber
+      g.fixture_replacement :factory_girl
     end
 GENERATORS
 
 application generators
 
-get "http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js",  "public/javascripts/jquery.js"
-get "http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.1/jquery-ui.min.js", "public/javascripts/jquery-ui.js"
+inside('public/javascripts') do
+	FileUtils.rm_rf %w(controls.js dragdrop.js effects.js prototype.js rails.js)
+end
+
+get "http://code.jquery.com/jquery-latest.min.js",  "public/javascripts/jquery.js"
 get "http://github.com/rails/jquery-ujs/raw/master/src/rails.js", "public/javascripts/rails.js"
 
 gsub_file 'config/application.rb', 'config.action_view.javascript_expansions[:defaults] = %w()', 'config.action_view.javascript_expansions[:defaults] = %w(jquery.js jquery-ui.js rails.js)'
 
 create_file "log/.gitkeep"
 create_file "tmp/.gitkeep"
-create_file "app/views/pages/.gitkeep"
+
+run "rm public/index.html"
+
+generate 'cucumber:install', '--capybara', '--testunit', '--spork'
 
 git :init
 git :add => "."
